5d1f37c267f5a248568c75ba43cc6e36336ba41a
Alicia Sykes <sykes.alicia@gmail.com>
2016-05-15 09:45:45 +0100

    Production code

---
+++ client_side_production/javascripts/page-actions.js
@@ -0,0 +1,2 @@
+$(function(){return $("nav").hide(),$(window).scroll(function(){return $(this).scrollTop()>100?$("nav").fadeIn():$("nav").fadeOut()})}),$("#get-started").click(function(){return $(this).addClass("circle").html("Start Speaking").append('<br><i class="material-icons">pause</i>'),startRecording(),$("#theInput").slideDown(400)});
+/* (C) Alicia Sykes <aliciasykes.com> MIT License. */
