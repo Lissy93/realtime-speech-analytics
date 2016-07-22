35930aa689c4da17ba39aec3dd594fdf7b0b7def
Alicia Sykes <sykes.alicia@gmail.com>
2016-07-22 12:53:52 +0100

    New project structure

--- app.js
+++ app.js
@@ -5,25 +5,24 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./production/server/page-routes/index');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'source/views'));
 app.set('view engine', 'jade');
 
-//app.use(favicon(__dirname + '/client_side_production/favicon.ico'));
+app.use(favicon(__dirname + '/production/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client_side_production')));
+app.use(express.static(path.join(__dirname, 'production')));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.use('/', routes);
-app.use('/api/entity', require('./routes/api-entity-extraction'));
+app.use('/api/entity', require('./production/server/api-routes/api-entity-extraction'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,7 +38,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render('./production/server-side-client-side-scripts/error', {
       message: err.message,
       error: err
     });
